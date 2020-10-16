6654879062f67637a011178e5d9a342b3501122d
Azat Mursiev <azatmursiev@gmail.com>
2020-10-16 10:30:02 +0500

    Add gulp-sourcemaps

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,9 +1,12 @@
 const {src, dest} = require('gulp')
 sass = require('gulp-sass')
+sourcemaps = require('gulp-sourcemaps')
 
 module.exports = styles = () => {
     return src('app/sass/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(dest('build/css'))
     .pipe(browserSync.stream())
 }
